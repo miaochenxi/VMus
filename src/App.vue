d277f98eb7b6751e1a7f43eaa46794339e33bd5e
#Undefined_Mcx <32982052+miaochenxi@users.noreply.github.com>
2020-11-27 22:27:59 +0800

    配置项目

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <navigation-bar></navigation-bar>
+    <router-view />
   </div>
-  <router-view/>
 </template>
-
+<script>
+import NavigationBar from './components/NavigationBar.vue'
+export default {
+  components: { NavigationBar }
+}
+</script>
 <style lang="scss">
+body {
+  background: black;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+  font-size: 20px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  color: white;
 }
 </style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="grid grid-cols-3 gap-4" id="nav">
+    <div class="inline-flex col-start-1">
+      <img class="h-8 inline" alt="Vue logo" src="../assets/logo.png" />
+      <span class="text-2xl">VMusic</span>
+    </div>
+    <div class="col-start-2" id="router">
+      <router-link to="/">首页</router-link>
+      <router-link to="/about">探索</router-link>
+      <router-link to="/about">媒体库</router-link>
+    </div>
+    <img alt="User Pic" class="w-9 col-end-4 justify-self-end" src="../assets/images/user.svg">
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+#nav {
+  padding: 8px;
+  a {
+    color: #FFFFFF80;
+
+    &.router-link-exact-active {
+      color:white;
+    }
+  }
+}
+</style>
